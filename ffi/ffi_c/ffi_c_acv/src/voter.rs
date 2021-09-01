bdba188836085188f611b3b38c2dc2c13ce6f121
HaoXuan40404 <444649358@qq.com>
2021-09-01 11:18:45 +0800

    remove acv ffi

--- ffi/ffi_c/ffi_c_acv/src/voter.rs
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 WeDPR Lab Project Authors. Licensed under Apache-2.0.
-
-//! Library of macros and functions for FFI of a voter in ABC
-//! solution, targeting C/C++ compatible architectures (including iOS).
-
-// C/C++ FFI: C-style interfaces will be generated.
